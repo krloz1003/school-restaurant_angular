be97c43ece9ea7f5dc163e2e3050bf3872ac8a5d
Carlos Avila Aguilar <krloz1003@gmail.com>
2018-09-11 23:05:53 -0500

    Configurando el menu en la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { RouterModule, Routes } from  '@angular/router';
+import { IndexComponent } from './customers/index/index.component';
 import { AppComponent } from './app.component';
 // Importando y registrando (NgModule->imports) mi componente Customers
 import { CustomersModule } from './customers/customers.module';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
